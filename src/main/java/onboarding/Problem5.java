dea74145c75fcdb685018997d002e2ad69cdefaa
ssunnykku <ssunnykku@gmail.com>
2023-10-17 17:25:56 +0900

    feat: Problem5

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,24 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+
+        int[] m = {50000,10000,5000,1000,500,100,50,10,1};
+        Integer[] cnt = {0,0,0,0,0,0,0,0,0};
+        List<Integer> answer = Arrays.asList(cnt);
+        // money가 m보다 클 때
+        for(int i=0; i<m.length; i++) {
+            while (m[i] <= money) {
+                money -= m[i];
+                cnt[i] += 1;
+            }
+        }
         return answer;
     }
+
 }
