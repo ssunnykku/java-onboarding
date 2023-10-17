6e159dd1833dd9ec573fc7f6a7b9eea5b3bdc0ab
ssunnykku <ssunnykku@gmail.com>
2023-10-17 14:03:18 +0900

    feat: Problem3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,23 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+
+        for(int i=1; i<number+1; i++) {
+            String str = Integer.toString(i);
+            String[] arr = str.split("");
+
+            answer += Collections.frequency(Arrays.asList(arr),"3");
+            answer += Collections.frequency(Arrays.asList(arr),"6");
+            answer += Collections.frequency(Arrays.asList(arr),"9");
+        }
         return answer;
     }
+
 }
