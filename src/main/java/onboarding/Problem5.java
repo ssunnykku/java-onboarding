8c5a57d0b755d6b197fdee36a55e164a7d2b11dc
ssunnykku <ssunnykku@gmail.com>
2023-10-18 15:03:27 +0900

    feat: problem5 조건문 삭제

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,26 +1,27 @@
 package onboarding;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Problem5 {
     public static List<Integer> solution(int money) {
 
-        int[] m = {50000,10000,5000,1000,500,100,50,10,1};
-        Integer[] cnt = {0,0,0,0,0,0,0,0,0};
-        List<Integer> answer = Arrays.asList(cnt);
+        int[] arr = {50000,10000,5000,1000,500,100,50,10,1};
+        List<Integer> answer = new ArrayList<>();
 
-        for(int i=0; i<m.length; i++) {
-           if (money >= m[i]) {
-              cnt[i] += money / m[i];
-              money = money % m[i];
-           }
+        for(int num : arr) {
+            int cnt = money / num;
+            money = money % num;
+            answer.add(cnt);
         }
+
         return answer;
     }
 
+
     public static void main(String[] args) {
-        System.out.println(solution(15000));
+        System.out.println(solution(50237));
     }
 
 }
