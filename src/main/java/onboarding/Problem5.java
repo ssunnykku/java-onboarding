c1b57a6b8e1f1a75fa230118d09f36ed91421535
ssunnykku <ssunnykku@gmail.com>
2023-10-18 14:49:39 +0900

    fix: problem5 이중반복문 수정

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,8 +1,6 @@
 package onboarding;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Problem5 {
@@ -11,14 +9,18 @@ public class Problem5 {
         int[] m = {50000,10000,5000,1000,500,100,50,10,1};
         Integer[] cnt = {0,0,0,0,0,0,0,0,0};
         List<Integer> answer = Arrays.asList(cnt);
-        // money가 m보다 클 때
+
         for(int i=0; i<m.length; i++) {
-            while (m[i] <= money) {
-                money -= m[i];
-                cnt[i] += 1;
-            }
+           if (money >= m[i]) {
+              cnt[i] += money / m[i];
+              money = money % m[i];
+           }
         }
         return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(15000));
     }
 
 }
